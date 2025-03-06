292eb8ccb2ef7e6841baafd07ce8bb260f8caa90
Oluwuyi Olumide <137781836+mideyolu@users.noreply.github.com>
2025-03-06 07:44:41 +0100

    Update api.jsx

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // Create Axios instance
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000/",
-    //baseURL: import.meta.env.VITE_BACKEND_URL,
+    //baseURL: "http://127.0.0.1:8000/",
+    baseURL: import.meta.env.VITE_BACKEND_URL,
     headers: {
         "Content-Type": "application/json",
     },
